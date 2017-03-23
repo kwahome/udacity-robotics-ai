c717fda8413a4aeb9100dbf34551dce4d63b5b90
kwahome <kevowahome@gmail.com>
2017-03-23 22:18:35 +0300

    Particle filters

---
+++ particle-filters/moving_robot.py
@@ -0,0 +1,27 @@
+# Make a robot called myrobot that starts at
+# coordinates 30, 50 heading north (pi/2).
+# Have your robot turn clockwise by pi/2, move
+# 15 m, and sense. Then have it turn clockwise
+# by pi/2 again, move 10 m, and sense again.
+#
+# Your program should print out the result of
+# your two sense measurements.
+#
+# Don't modify the code below. Please enter
+# your code at the bottom.
+
+from math import *
+import random
+import Robot
+
+
+
+landmarks  = [[20.0, 20.0], [80.0, 80.0], [20.0, 80.0], [80.0, 20.0]]
+world_size = 100.0
+
+
+
+####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
+
+myrobot = robot()
+
